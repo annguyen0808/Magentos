cbbcef1096e4f70a5e873c6bf2f91430c06dfb75
Nguyen-An <Less>
2019-07-29 22:29:56 +0700

    first commit

---
+++ AHT/Blog/registration.php
@@ -0,0 +1,8 @@
+<?php
+  \Magento\Framework\Component\ComponentRegistrar::register(
+      \Magento\Framework\Component\ComponentRegistrar::MODULE,
+      'AHT_Blog',
+      __DIR__
+  );
+
+  // thư mục này đăng ký tên cho các module
---
+++ MyCompany/ExampleAdminNewPage/registration.php
@@ -0,0 +1,8 @@
+<?php
+  \Magento\Framework\Component\ComponentRegistrar::register(
+      \Magento\Framework\Component\ComponentRegistrar::MODULE,
+      'MyCompany_ExampleAdminNewPage',
+      __DIR__
+  );
+
+  // thư mục này đăng ký tên cho các module
