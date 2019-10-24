8f5024b379fe54df9b3b1b740506a8bb9f25c884
Nguyen-An <Less>
2019-10-24 23:50:41 +0700

    first commit

--- Van/Baitap/Block/Index.php
+++ Van/Baitap/Block/Index.php
@@ -1,22 +1,53 @@
-<?php 
+<?php
+
 namespace Van\Baitap\Block;
 
-class Index extends \Magento\Framework\View\Element\Template
-{  
-
-
-public function __construct(
-    \Magento\Framework\View\Element\Template\Context $context ) 
+Class Index extends \Magento\Framework\View\Element\Template
+{
+	protected $dataHelper;
+	
+	public function __construct(
+		\Magento\Framework\View\Element\Template\Context $context,
+		\Van\Baitap\Helper\Data $dataHelper,
+		\Magento\Store\Model\StoreManagerInterface $storeManager
+	){
+		parent::__construct($context);
+		$this->dataHelper = $dataHelper;
+		$this->_storeManager = $storeManager;
+	}
+	
+	public function getTitle()
 	{
+		$title = $this->dataHelper->getGeneralConfig('title');
+		
+		return $title;
+	}
+	
 
-         parent::__construct($context);
-    }
-
-    public function getHelloWorld()
+	public function getTextarea()
 	{
-		return __("HelloWorld");
+		$textarea = $this->dataHelper->getGeneralConfig('textarea');
+		
+		return $textarea;
+	}
+	
+
+	public function getImage()
+	{
+		$image = $this->dataHelper->getGeneralConfig('image');
+		
+		return $this->getUrlMedia().$image;
 	}
 
 
+	public function getBaseUrl()
+	{
+		return $this->_storeManager->getStore()->getBaseUrl();
+	}
+
+	public function getUrlMedia()
+	{
+         $mediaUrl = $this ->_storeManager-> getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA );
+         return $mediaUrl."baitap/";
+	}
 }
-?>
